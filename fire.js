7cc8f12589c30c3196ba6bd4917d78e8591459f9
EoqLp <feliperangel96@hotmail.com>
2019-06-29 02:06:45 -0300

    Estrutura inicial

---
+++ fire.js
@@ -0,0 +1,45 @@
+//https://youtu.be/fxm8cadCqbs?t=571
+
+const firePixels = [];
+const fireWidth = 2;
+const fireHeight = 3;
+
+function init(){
+    createED();
+    renderFire();
+}
+
+function createED(){
+    const numberPixels = fireWidth * fireHeight
+
+    for(let i = 0; i < numberPixels; i++){
+        firePixels[i] = 0;
+    }
+}
+
+function createPropagation(){
+
+}
+
+function renderFire(){
+    let html = '<table cellpadding=0 cellspacing=0>'
+
+    for(let row = 0; row < fireHeight; row++){
+        html += '<tr>'
+
+        for (let column = 0; column < fireWidth; column++){
+            const pixelIndex = column + (fireWidth * row)
+            html += '<td>'
+            html += pixelIndex
+            html += '</td>'
+        }
+
+        html += '</tr>'
+    }
+
+    html += '</table>'
+
+    document.querySelector('#fireCanvas').innerHTML = html
+}
+
+init()
